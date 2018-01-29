c167fee5bc08f6439753386286db5b24dbc6832e
shadowfox <cuiyingjia@foxmail.com>
2018-01-29 23:44:04 +0800

    add mysql

--- bootdjg/settings.py
+++ bootdjg/settings.py
@@ -77,8 +77,15 @@ WSGI_APPLICATION = 'bootdjg.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
+        # 'NAME': 'ip_db',
+        'NAME': 'foxdjg',
+        'USER':'udjg',
+        'PASSWORD':'shadowfox',
+        'HOST':'127.0.0.1',
+        'PORT':'3306',
     }
 }
 
